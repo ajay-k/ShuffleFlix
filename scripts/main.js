f1dc8cdcaad3a28812a1a1069d708d4bb4b41ce1
ajay-k <ajay_k@sbcglobal.net>
2020-06-30 21:44:09 -0700

    Adds multiple shows

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,35 @@
 $( document ).ready(function() {
   console.log( "ready!" );
   let abc = "abc"
+  let netflixId = 70136120
+  let textVal = $("#netflixIDText").val()
+
+  // if( $("#netflixIDText").val() == null){
+  //   textVal = 70136120
+  // }else{
+  //   textVal = $("#netflixIDText").val()
+
+  // }
+  // console.log(textVal)
+
   //edee
   // dsdfsfd
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://unogsng.p.rapidapi.com/episodes?netflixid=${textVal}`,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "unogsng.p.rapidapi.com",
+      "x-rapidapi-key": "e51b945689mshdac2c3f84cb6376p1e9eb2jsnb59224aee908"
+    }
+  }
+
   $("#btnClick").click(function(){
+    // alert(textVal)
     $.ajax(settings).done(function (response) {
+      console.log("REsponse")
       console.log(response);
       let randomSeason =  Math.floor(Math.random() * (response.length - 0) + 0);
       console.log(randomSeason)
@@ -12,22 +37,59 @@ $( document ).ready(function() {
       console.log(response[randomSeason].episodes[randomEp])
 
       document.getElementById("title").innerHTML = response[randomSeason].episodes[randomEp].title
-      document.getElementById("link").innerHTML = "https://www.netflix.com/watch/" + response[randomSeason].episodes[randomEp].epid
+      document.getElementById("link").href= "https://www.netflix.com/watch/" + response[randomSeason].episodes[randomEp].epid
+      document.getElementById("synopsis").innerHTML= response[randomSeason].episodes[randomEp].synopsis
+      document.getElementById("epImg").src= response[randomSeason].episodes[randomEp].img
+      document.getElementById("season-title").innerHTML= `Season ${randomSeason} | Episode ${randomEp}`
+
+      console.log(settings)
+
+      // document.getElementById("link").innerHTML = 
     });
    });
 
+   $("#netflixIdBtn").click(function(){
+    //  alert("HELLO");
+    //  alert(textVal)
+     console.log("Button clicked");
+     textVal = $("#netflixIDText").val()
+     netflixId = textVal
+     var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": `https://unogsng.p.rapidapi.com/episodes?netflixid=${netflixId}`,
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "unogsng.p.rapidapi.com",
+        "x-rapidapi-key": "e51b945689mshdac2c3f84cb6376p1e9eb2jsnb59224aee908"
+      }
+      }
+
+      $.ajax(settings).done(function (response) {
+        console.log("REsponse")
+        console.log(response);
+        let randomSeason =  Math.floor(Math.random() * (response.length - 0) + 0);
+        console.log(randomSeason)
+        let randomEp = Math.floor(Math.random() * (response[randomSeason].episodes.length - 0) + 0);
+        console.log(response[randomSeason].episodes[randomEp])
+  
+        document.getElementById("title").innerHTML = response[randomSeason].episodes[randomEp].title
+        document.getElementById("link").href= "https://www.netflix.com/watch/" + response[randomSeason].episodes[randomEp].epid
+        document.getElementById("synopsis").innerHTML= response[randomSeason].episodes[randomEp].synopsis
+        document.getElementById("epImg").src= response[randomSeason].episodes[randomEp].img
+        document.getElementById("season-title").innerHTML= `Season ${randomSeason} | Episode ${randomEp}`
+  
+        console.log(settings)
+  
+        // document.getElementById("link").innerHTML = 
+      });
+      console.log(settings)
+
+
+    });
 
       
-   var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://unogsng.p.rapidapi.com/episodes?netflixid=70136120",
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "unogsng.p.rapidapi.com",
-      "x-rapidapi-key": "e51b945689mshdac2c3f84cb6376p1e9eb2jsnb59224aee908"
-    }
-  }
+
   
   // $.ajax(settings).done(function (response) {
   //   console.log(response);
@@ -35,6 +97,14 @@ $( document ).ready(function() {
   //   console.log(randomSeason)
   //   let randomEp = Math.floor(Math.random() * (response[randomSeason].episodes.length - 0) + 0);
   //   console.log(response[randomSeason].episodes[randomEp])
+
+  //   document.getElementById("title").innerHTML = response[randomSeason].episodes[randomEp].title
+  //   document.getElementById("link").href= "https://www.netflix.com/watch/" + response[randomSeason].episodes[randomEp].epid
+  //   document.getElementById("synopsis").innerHTML= response[randomSeason].episodes[randomEp].synopsis
+  //   document.getElementById("epImg").src= response[randomSeason].episodes[randomEp].img
+
+  //   // document.getElementById("link").innerHTML = 
   // });
+ 
   
 });
